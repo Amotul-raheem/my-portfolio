67770a784031238721d08cd3c473eafb9a0426b8
Amotul-raheem <amotulraheem1499@gmail.com>
2024-08-31 19:49:38 +0100

    init

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between ">
+            <div className="absolute filter backdrop-blur-[3px] w-full h-screen"></div>
+            <div className="flex justify-start overflow-hidden my-12 z-10">
+                <h1> CREATIVE  FRONTEND DEVELOPER</h1>
+                <h1> CREATIVE  FRONTEND DEVELOPER</h1>
+                <h1> CREATIVE  FRONTEND DEVELOPER</h1>
+            </div>
+        </main>
+    )
+}
